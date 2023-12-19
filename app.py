92b24826da18ee04a4d81d6b5c43d35e4d9c2a9d
kumarks1122 <kumar.kandasamy@tarento.com>
2023-12-19 17:49:52 +0530

    #000 | updated app file

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ sockets.register_blueprint(audio_socket)
 HTTP_SERVER_PORT = 8000
 
 print("Server listening on: http://localhost:" + str(HTTP_SERVER_PORT))
-print("Route for media: http://localhost:" + str(HTTP_SERVER_PORT) + '/media')
+print("Route for media: ws://localhost:" + str(HTTP_SERVER_PORT) + '/media')
 
 if __name__ == '__main__':
     server = pywsgi.WSGIServer(('localhost', HTTP_SERVER_PORT), app, handler_class=WebSocketHandler)
