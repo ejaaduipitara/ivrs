a4efa9c15599e6e2d6c6254678955f070d15fe0f
kumarks1122 <kumar.kandasamy@tarento.com>
2024-02-06 17:12:07 +0530

    #000 | NodeJS telemetry generation and service fixes added

--- nodejs/voice-streaming/index.js
+++ nodejs/voice-streaming/index.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const crypto = require('crypto');
+const bodyParser = require('body-parser')
+
 const axios = require('axios')
+const Telemetry = require('./telemetry')
 const app = express()
+
 const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+const CATEGORIES = ["story", "song", "question"]
 
 function getDayOfYear(date) {
   const start = new Date(date.getFullYear(), 0, 0);
@@ -11,8 +21,8 @@ function getDayOfYear(date) {
 }
 
 async function getAudio(audioKey) {
-  const urlData = process.env.IVRS_CONFIG_URL;
-  // const urlData = "https://objectstorage.ap-hyderabad-1.oraclecloud.com/n/ax2cel5zyviy/b/sbdjp-ivrs/o/audio/ivrs_config.json"
+  // const urlData = process.env.IVRS_CONFIG_URL;
+  const urlData = "https://objectstorage.ap-hyderabad-1.oraclecloud.com/n/ax2cel5zyviy/b/sbdjp-ivrs/o/audio/ivrs_config.json"
 
   try {
     getDayOfYear(new Date())
@@ -37,9 +47,17 @@ async function getAudio(audioKey) {
 }
 
 
-app.get('/media', async (req, res) => {
-  const language = req.query.language
-  const category = req.query.category
+app.post('/media', async (req, res) => {
+  let sessionId = req.body.call_id
+  var did = crypto.createHash('md5').update(req.body.caller_id_number).digest('hex');
+  let telemetry = new Telemetry(sessionId, did)
+
+  let userInputFlow = req.body.call_flow
+  const dtmfObj = userInputFlow.pop()
+  const languageObj = userInputFlow.pop()
+  
+  category = CATEGORIES[parseInt(dtmfObj.input)]
+  language = languageObj.name.trim().toLowerCase()
   audioKey = `${category}:${language}`
   audioUrl = await getAudio(audioKey)
 
@@ -53,7 +71,25 @@ app.get('/media', async (req, res) => {
     audioUrl = await getAudio(audioKey)
   }
 
-  res.send({url: audioUrl});
+  resultObj = [{
+    "recording": {
+      "type": "url",
+      "data": audioUrl
+    }
+  }]
+
+  startEdata = {
+    "input": parseInt(dtmfObj.input),
+    "language": language,
+    "audio_type": category,
+    "audio_name": audioUrl
+  }
+  telemetry.start(startEdata)
+
+  telemetry.end()
+  telemetry.push()
+
+  res.send(resultObj);
 })
 
 app.listen(port, () => {
